a88b29f010b1165a76fc73b6c11244abad34ba10
Muhammad nazim fu Bin asmadi <muhammadnazimfu@gmail.com>
2025-07-15 13:22:43 +0000

    Instead of making the whole website scrollable, make the scroll inside t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,7 @@ export default function DashboardPage() {
   };
 
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+    <div className="flex h-screen w-full flex-col bg-muted/40">
       <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 py-4">
         <Logo />
         <div className="ml-auto flex items-center gap-4">
@@ -123,7 +123,7 @@ export default function DashboardPage() {
             </Avatar>
         </div>
       </header>
-      <main className="flex flex-1 flex-col gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+      <main className="flex flex-1 flex-col gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 overflow-auto">
         <Tabs defaultValue="overview">
           <div className="flex items-center">
             <TabsList>
